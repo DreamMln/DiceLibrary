28e34487b70a0a4c1ce0eda23518a85ecbf47fef
DreamMln <maix2374@edu.zealand.dk>
2022-08-20 13:09:07 +0200

    Add project files.

---
+++ DiceLib29aug/DiceResult.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace DiceLib29aug
+{
+    public class DiceResult
+    {
+        //repræsentere en spillers navn og resultat af terningkast
+        private string _playerName;
+        private int _diceValue;
+
+        //hvis værdien er mindre en 3, så kast en ex!
+        public string PlayerName { 
+            get { return _playerName; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                if (value.Length < 3)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                //ellers sæt værdien
+                _playerName = value;
+            }
+        }
+        //hvis værdien er under 1, og værdien er over 6, så kast en ex!
+        public int DiceValue
+        {
+            get { return _diceValue; }
+            set
+            {
+                if (value < 1 || value > 6)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                //ellers sæt værdien
+                _diceValue = value;
+            }
+        }
+
+        public DiceResult()
+        {
+            //tom con, for (de)serialization
+        }
+        public DiceResult(string _playerName, int _diceValue)
+        {
+            PlayerName = _playerName;
+            DiceValue = _diceValue;
+        }
+        public override string ToString()
+        {
+            return $"Player name is: {PlayerName} & Dice value is: {DiceValue}";
+        }
+    }
+}
---
+++ DiceLib29augTests/DiceResultTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DiceLib29aug;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceLib29aug.Tests
+{
+    [TestClass()]
+    public class DiceResultTests
+    {
+        //unit test af modelklassen diceresult, vi tester
+        //på om de givne constraints virker som forventet
+
+        [TestMethod()]
+        public void DiceTestConstructor()
+        {
+            //testc data - Arrange
+            DiceResult diceTest = new DiceResult("Kuno", 4);
+
+            Assert.AreEqual(4, diceTest.DiceValue);
+            Assert.AreEqual("Kuno", diceTest.PlayerName);
+        }
+
+        [TestMethod()]
+        public void DiceTestValid()
+        {
+            //test data - Arrange
+            DiceResult diceTest = new DiceResult("Ken",3);
+            DiceResult diceTest1 = new DiceResult("Lone", 6);
+
+            Assert.AreEqual(3, diceTest.DiceValue);
+            Assert.AreEqual("Ken", diceTest.PlayerName);
+
+            Assert.AreEqual(6, diceTest1.DiceValue);
+            Assert.AreEqual("Lone", diceTest1.PlayerName);
+        }
+        [TestMethod()]
+        public void DiceTestInvalid()
+        {
+            //test data - Arrange
+            DiceResult diceTest = new DiceResult("Ken", 3);
+
+            //test null ex
+            Assert.ThrowsException<ArgumentNullException>(() => diceTest.PlayerName = null);
+            //test over/under grænsen - kast ex!
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => diceTest.PlayerName = "Bo");
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => diceTest.DiceValue = 0);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => diceTest.DiceValue = 7);
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            //test data - Arrange
+            DiceResult diceTest = new DiceResult("Ken", 3);
+
+            string testSize = diceTest.ToString();
+            Assert.AreEqual("Player name is: Ken & Dice value is: 3", testSize);
+            //return $"Dice value is: {DiceValue} & Player name is: {PlayerName}";
+
+        }
+    }
+}
